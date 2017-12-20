35c7239863a6145a54d1fd02ab04dac06f450f50
Tao Xiong <xt2357@gmail.com>
2017-12-20 17:21:18 +0800

    color supported

--- make.py
+++ make.py
@@ -4,9 +4,33 @@ import subprocess
 import sys
 import os
 
+
+def enum(*sequential, **named):
+    enums = dict(zip(sequential, range(len(sequential))), **named)
+    return type('Enum', (), enums)
+
+COLORS = enum (
+    HEADER = '\033[95m',
+    OKBLUE = '\033[94m',
+    OKGREEN = '\033[92m',
+    WARNING = '\033[93m',
+    FAIL = '\033[91m',
+    ENDC = '\033[0m',
+    BOLD = '\033[1m',
+    UNDERLINE = '\033[4m',
+)
+
 def call(cmd, print_cmd=False):
     if print_cmd:
         print(cmd)
+    cmd = cmd.replace(COLORS.HEADER, '')
+    cmd = cmd.replace(COLORS.OKBLUE, '')
+    cmd = cmd.replace(COLORS.OKGREEN, '')
+    cmd = cmd.replace(COLORS.WARNING, '')
+    cmd = cmd.replace(COLORS.FAIL, '')
+    cmd = cmd.replace(COLORS.ENDC, '')
+    cmd = cmd.replace(COLORS.BOLD, '')
+    cmd = cmd.replace(COLORS.UNDERLINE, '')
     if subprocess.call(cmd, shell=True, stderr=sys.stderr):
         sys.exit(1)
 
@@ -16,25 +40,25 @@ def compile(filename, to_path=None, tags=[]):
     if not to_path:
         to_path = os.path.dirname(filename)
     to_path = os.path.join(to_path, os.path.basename(filename))
-    cmd = 'gcc %s%s -o %s.o %s'%(filename, suffix, to_path, ' '.join(tags))
-    print('[COMPILE] -> %s'%(cmd))
+    cmd = 'gcc ' + COLORS.UNDERLINE + COLORS.BOLD + '%s%s'%(filename, suffix) + COLORS.ENDC + ' -o %s.o %s'%(to_path, ' '.join(tags))
+    print(COLORS.OKBLUE + '[COMPILE]' + COLORS.ENDC + ' -> %s'%(cmd))
     call(cmd)
     return to_path + '.o'
 
 def link(objects, output_filename, tags=[]):
     cmd = 'gcc '
     for obj in objects:
-        cmd += obj + ' '
+        cmd += COLORS.UNDERLINE + COLORS.BOLD + obj + COLORS.ENDC + ' '
     cmd += '-o %s'%output_filename
     cmd += ' %s' % (' '.join(tags))
-    print('[LINKING] -> %s'%(cmd))
+    print(COLORS.OKGREEN + '[LINKING]' + COLORS.ENDC + ' -> %s'%(cmd))
     call(cmd)
 
 def lib_static(objects, output_filename):
     cmd = 'ar rcs %s '%output_filename
     for obj in objects:
         cmd += obj + ' '
-    print('[ARCHIVE] -> %s'%(cmd))
+    print(COLORS.OKGREEN + '[ARCHIVE]' + COLORS.ENDC + ' -> %s'%(cmd))
     call(cmd)
 
 def make_debug():
