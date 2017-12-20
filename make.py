1ab3071cd77d11001a6ac605d42832b8f2f435f9
Tao Xiong <xt2357@gmail.com>
2017-12-20 16:09:22 +0800

    make.py changes

--- make.py
+++ make.py
@@ -17,7 +17,7 @@ def compile(filename, to_path=None, tags=[]):
         to_path = os.path.dirname(filename)
     to_path = os.path.join(to_path, os.path.basename(filename))
     cmd = 'gcc %s%s -o %s.o %s'%(filename, suffix, to_path, ' '.join(tags))
-    print('[COMPILE]-> %s: %s'%(filename + suffix, cmd))
+    print('[COMPILE] -> %s'%(cmd))
     call(cmd)
     return to_path + '.o'
 
@@ -27,21 +27,14 @@ def link(objects, output_filename, tags=[]):
         cmd += obj + ' '
     cmd += '-o %s'%output_filename
     cmd += ' %s' % (' '.join(tags))
-    print('[LINK]-> %s: %s'%(' '.join(objects), cmd))
+    print('[LINKING] -> %s'%(cmd))
     call(cmd)
 
 def lib_static(objects, output_filename):
     cmd = 'ar rcs %s '%output_filename
     for obj in objects:
         cmd += obj + ' '
-    print('[LIB_STATIC]-> %s: %s'%(' '.join(objects), cmd))
-    call(cmd)
-
-def lib_dynamic(objects, output_filename):
-    cmd = 'gcc -shared -o %s '%output_filename
-    for obj in objects:
-        cmd += obj + ' '
-    print('[LIB_DYNAMIC]-> %s: %s'%(' '.join(objects), cmd))
+    print('[ARCHIVE] -> %s'%(cmd))
     call(cmd)
 
 def make_debug():
@@ -63,13 +56,7 @@ def make_dynamic():
     for root, dirs, files in os.walk('src'):
         for f in [i for i in files if i.endswith('.cpp') and i != 'main.cpp']:
             objs.append(compile(os.path.join(root, f), 'objects', ['-fPIC', '-Wall', '-g', '-c', '-lstdc++', '-std=c++11']))
-    lib_dynamic(objs, 'lib/libco.so')
-    
-def make_clean():
-    call('rm -f objects/*.o', print_cmd=True)
-    call('rm -f lib/*.so', print_cmd=True)
-    call('rm -f *.out', print_cmd=True)
-    call('rm -f lib/*.a', print_cmd=True)
+    link(objs, 'lib/libco.so', ['-shared', '-ldl', '-lstdc++', '-std=c++11'])
 
 def make_test():
     make_dynamic()
@@ -77,9 +64,13 @@ def make_test():
     for root, dirs, files in os.walk('test'):
         for f in [i for i in files if i.endswith('.cpp')]:
             objs.append(compile(os.path.join(root, f), 'objects', ['-Wall', '-g', '-c', '-lstdc++', '-std=c++11']))
-    link(objs, 'test.out', ['-L./lib', '-lco', '-ldl', '-lstdc++', '-std=c++11', '-Wl,-rpath=./lib'])
-    # gcc XXX.c -o xxx.out -L$HOME/.usr/lib -lXX -Wl,-rpath=/home/user/.usr/lib
+    link(objs, 'test.out', ['-L./lib', '-lco', '-lstdc++', '-std=c++11', '-Wl,-rpath=./lib'])
 
+def make_clean():
+    call('rm -f objects/*.o', print_cmd=True)
+    call('rm -f lib/*.so', print_cmd=True)
+    call('rm -f *.out', print_cmd=True)
+    call('rm -f lib/*.a', print_cmd=True)
 
 if __name__=='__main__':
     if len(sys.argv) < 2:
